b4fd7f0e81bdbd135f33d71e4debfb57e5f0b9a1
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2022-01-10 19:53:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/juego_3enraya/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.juego_3enraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button btn1 = findViewById(R.id.button1);
+        Button btn2 = findViewById(R.id.button2);
+        Button btn3 = findViewById(R.id.button3);
+        Button btn4 = findViewById(R.id.button4);
+        Button btn5 = findViewById(R.id.button5);
+        Button btn6 = findViewById(R.id.button6);
+        Button btn7 = findViewById(R.id.button7);
+        Button btn8 = findViewById(R.id.button8);
+        Button btn9 = findViewById(R.id.button9);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
